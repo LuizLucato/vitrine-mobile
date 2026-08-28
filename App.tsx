41ea79afb14d3f6c4be089a961b9ccb79125c309
Luiz Francisco Lucato Piccolo <luizfranciscolucato@gmail.com>
2026-08-28 11:11:11 -0300

    aula01: projeto Expo com TypeScript rodando

--- App.tsx
+++ App.tsx
@@ -4,7 +4,10 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.titulo}>Vitrine</Text>
+      <Text style={styles.subtitulo}>
+        Luiz Francisco - Dispostivos Móveis 2026
+        </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +16,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#0F1B33',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  titulo: { color: '#611DAFB', fontSize: 34, fontWeight: 'bold',},
+  subtitulo: { color: '#FFFFFF', fontSize: 15, marginTop: 8},
 });
